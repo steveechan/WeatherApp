a1db176d2dbb131c557b1c8627ab1b95e660c490
stephen <liluzisteveee@gmail.com>
2022-05-14 18:37:02 -0400

    First commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search..." />
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      api_key: '5cf1db58ddb930cb1e8b19bdf7d7ee23'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
