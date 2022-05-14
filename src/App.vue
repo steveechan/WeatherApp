573beea04f15805f4ff6f50b012c8c26ad7dfa70
stephen <liluzisteveee@gmail.com>
2022-05-14 18:47:25 -0400

    search box, background

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,15 @@
       <div class="search-box">
         <input type="text" class="search-bar" placeholder="Search..." />
       </div>
+
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Kenilworth, NJ</div>
+          <div class="date"> Saturday 14 May 2022</div>
+        </div>
+      </div>
+
+
     </main>
   </div>
 </template>
@@ -37,5 +46,41 @@ body {
   background-size: cover;
   background-position: bottom;
   transition: 0.4s;
+}
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  color: #313131;
+  font-size: 20px;
+
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255,255,255,0.75);
+  border-radius: 16px 0px 16px 0px;
 }
 </style>
